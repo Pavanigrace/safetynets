59e4b6f3efa6b9ec18ae515691da6b06536ba26d
Pavanigrace <pavanigrace4@gmail.com>
2026-05-20 19:31:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function whatsapp() {
+  window.open("https://wa.me/917093506117", "_blank");
+}
+
+function sendLead(event) {
+  event.preventDefault();
+  alert("Thank you! We will contact you soon.");
+}
